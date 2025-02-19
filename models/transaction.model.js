07f360570c2c030a047a880a4c81bd9c2265605b
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-19 10:38:40 +0100

    Transaction Model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,26 +1,10 @@
 const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema(
-  {
-    desc: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    type: {
-      type: String,
-      enum: ["income", "expense"], // Seulement "income" ou "expense"
-      required: true,
-    },
-    date: {
-      type: Date,
-      default: Date.now, // Ajoute automatiquement la date de transaction
-    },
-  },
-  { timestamps: true } // Pour ajouter createdAt et updatedAt
-);
+const TransactionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  desc: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, enum: ["income", "expense"], required: true }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
