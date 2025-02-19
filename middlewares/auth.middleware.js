e814124158a32ec4824e4e849d61416c72a9c336
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-19 10:35:35 +0100

    Auth Middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Accès refusé. Aucun token fourni." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token invalide ou expiré." });
+  }
+};
+
+module.exports = verifyToken;
